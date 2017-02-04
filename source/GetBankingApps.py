df441358c9766e31037ab2ab75417c1a0754154d
srikanthlogic <srik.lak@gmail.com>
2017-02-04 06:47:02 +0530

    First pass of all apps developed by Bank-Tech / Pay-Tech developer | developer vendors of banks

--- source/GetBankingApps.py
+++ source/GetBankingApps.py
@@ -5,15 +5,13 @@ def GetAppsByDeveloper(developer):
 	global applist
 	applist = []
 	dev_id_url = 'https://play.google.com/store/apps/developer?id='
-	#url = dev_id_url + 'State+Bank+of+India';
-	#url = dev_id_url + 'ICICI+Bank+Ltd.';
 
+	#url = dev_id_url + 'State+Bank+of+India';
 	url = dev_id_url + developer
 
 	doc = urllib2.urlopen(url)
 	soup = BeautifulSoup(doc, 'html.parser')
 	apps = soup.find_all("a","card-click-target")
-
 
 	for app in apps:
 		if app.get('href').split('id=')[1] not in applist:
@@ -21,7 +19,7 @@ def GetAppsByDeveloper(developer):
 	print '\n'.join(applist)
 
 def GetDeveloper(appname):
-	print appname
+	#print appname
 	doc = urllib2.urlopen('https://play.google.com/store/apps/details?id='+appname)
 	soup = BeautifulSoup(doc, 'html.parser')
 	return soup.find_all("a","document-subtitle primary")[0].find('span').contents[0].replace(' ','+')
@@ -35,5 +33,14 @@ def GetUPIDevelopers():
 			GetAppsByDeveloper(developer)
 	return
 
+def GetAppsByAllDeveloper():
+	with open('../input/PayTech-AppDevelopers.csv','r') as f:
+		for line in f.readlines():
+			#print line[:-1]
+			GetAppsByDeveloper(line[:-1])
+
 if __name__ == "__main__":
-	GetUPIDevelopers()
+	#GetUPIDevelopers()
+	GetAppsByAllDeveloper()
+
+
